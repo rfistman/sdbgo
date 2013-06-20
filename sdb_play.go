f2a9d02ede4d6b4312c1d94c935479bf18271acd
Rhythmic Fistman <rfistman@gmail.com>
2013-06-20 23:30:02 +1000

    put, top 10 select

--- sdb_play.go
+++ sdb_play.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"./s3go" // import straight from github? commit?
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
-	"strconv"
+	// "strconv"
 	"time"
 )
 
@@ -40,26 +41,38 @@ func main() {
 	cred := &s3.SecurityCredentials{AWSAccessKeyId: os.Getenv("AWS_ACCESS_KEY"), AWSSecretAccessKey: os.Getenv("AWS_SECRET_KEY")}
 
 	//2. get xml response
-	//3. PutAttributes (still a GET)
 	m := s3.Strmap{
 		"Action": "ListDomains",
 	}
 
-	r := s3.NewSDBRequest("GET", m)
+	r := s3.NewSDBRequest(m)
 	dothing(r, cred)
+
+	nplays := 1234
 
 	m = s3.Strmap{
-		"Action":     "PutAttributes",
-		"DomainName": "bn_bs",
-		//"Attribute.1.Name":  "slug",
-		//"Attribute.1.Value": "12afc3",
-		"ItemName":          "12afcd",
-		"Attribute.1.Name":  "upload_date",
-		"Attribute.1.Value": strconv.FormatInt(time.Now().Unix(), 10),
+		"Action":           "PutAttributes",
+		"DomainName":       "bn_bs",
+		"ItemName":         "1777xy",
+		"Attribute.1.Name": "upload_date",
+		//"Attribute.1.Value": strconv.FormatInt(time.Now().Unix(), 10),
+		// lexographically sortable date. not exactly RFC8601 but looks ok to me
+		// http://docs.aws.amazon.com/AmazonSimpleDB/latest/DeveloperGuide/Dates.html
+		// choose UTC/GMT so dates in different timezones are comparable
+		"Attribute.1.Value": time.Now().UTC().Format(time.RFC3339),
+		// lexographically pad out for 1billion plays
 		"Attribute.2.Name":  "plays",
-		"Attribute.2.Value": "0",
+		"Attribute.2.Value": fmt.Sprintf("%.9d", nplays),
 	}
-	r = s3.NewSDBRequest("GET", m)
+	r = s3.NewSDBRequest(m)
 	dothing(r, cred)
 
+	// sort attr must must appear in predicate
+	top10 := "select * from `bn_bs` where `plays` is not null order by `plays` desc limit 10"
+	m = s3.Strmap{
+		"Action":           "Select",
+		"SelectExpression": top10,
+	}
+	r = s3.NewSDBRequest(m)
+	dothing(r, cred)
 }
